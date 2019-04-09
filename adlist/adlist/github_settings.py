e3023d87f9892a5dbafdda3c89f180ead79c0696
AJ Estes <ajestes@umich.edu>
2019-04-09 20:50:32 +0000

    Adlist Finished

---
+++ adlist/adlist/github_settings.py
@@ -0,0 +1,26 @@
+
+# Copy this file to github_settings.py (don't check it into github)
+
+# Go to https://github.com/settings/developers
+
+# Add a New OAuth2 App
+
+# Using ngrok is hard because the url changes every time you start ngrok
+
+# If you are running on localhost, here are some settings:
+
+# Application name: Django on Desktop
+# Homepage Url: http://localhost:8000
+# Application Description: Whatever
+# Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
+
+
+# Using PythonAnywhere here are some settings:
+# Homepage Url: https://drchuck.pythonanywhere.com
+# Application Description: Whatever
+# Authorization callback URL: https://drchuck.pythonanywhere.com/oauth/complete/github/
+
+# Then copy the client_key and secret to this file
+
+SOCIAL_AUTH_GITHUB_KEY = 'b8070332ab48ecea00d6'
+SOCIAL_AUTH_GITHUB_SECRET = '11ea6bef088d5224c3922c5217e592c7c61b56cc'
--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -7,7 +7,9 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.core.files.uploadedfile import InMemoryUploadedFile
 
 from ads.models import Ad
+from ads.models import Comment
 from ads.forms import CreateForm
+from ads.forms import CommentForm
 
 # Create your views here.
 
@@ -21,6 +23,12 @@ class AdListView(AdsListView):
 class AdDetailView(AdsDetailView):
     model = Ad
     template_name = "ad_detail.html"
+    def get(self, request, pk) :
+        ad = Ad.objects.get(id=pk)
+        comments = Comment.objects.filter(ad=ad).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = { 'ad' : ad, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
 
 class AdCreateView(AdsCreateView):
     model = Ad
@@ -65,6 +73,23 @@ class AdFormView(LoginRequiredMixin, View):
         ad.save()
         return redirect(self.success_url)
 
+class CommentCreateView(AdsCreateView):
+    def post(self, request, pk) :
+        f = get_object_or_404(Ad, id=pk)
+        comment_form = CommentForm(request.POST)
+
+        comment = Comment(text=request.POST['comment'], owner=request.user, ad=f)
+        comment.save()
+        return redirect(reverse_lazy('ad_detail', args=[pk]))
+
+class CommentDeleteView(AdsDeleteView):
+    model = Comment
+    template_name = "comment_delete.html"
+
+    # def get_success_url(self):
+    #     ad = self.object.ad
+    #     return reverse_lazy('ad_detail', args=[forum.id])
+
 def stream_file(request, pk) :
     ad = get_object_or_404(Ad, id=pk)
     response = HttpResponse()
@@ -72,3 +97,47 @@ def stream_file(request, pk) :
     response['Content-Length'] = len(ad.picture)
     response.write(ad.picture)
     return response
+
+class ThingListView(AdsListView):
+    model = Ad
+    template_name = "ad_list.html"
+
+    def get(self, request) :
+        ad_list = Ad.objects.all()
+        favorites = list()
+        if request.user.is_authenticated:
+            # rows = [{'id': 2}]  (A list of rows)
+            rows = request.user.favorite_ads.values('id')
+            favorites = [ row['id'] for row in rows ]
+        ctx = {'ad_list' : ad_list, 'favorites': favorites}
+        return render(request, self.template_name, ctx)
+
+# https://stackoverflow.com/questions/16458166/how-to-disable-djangos-csrf-validation
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.db.utils import IntegrityError
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Add PK",pk)
+        t = get_object_or_404(Ad, id=pk)
+        ad = Ad(user=request.user, ad=t)
+        try:
+            ad.save()  # In case of duplicate key
+        except IntegrityError as e:
+            pass
+        return HttpResponse()
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DeleteFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Delete PK",pk)
+        t = get_object_or_404(Ad, id=pk)
+        try:
+            ad = Ad.objects.get(user=request.user, ad=t).delete()
+        except Ad.DoesNotExist as e:
+            pass
+
+        return HttpResponse()
+
